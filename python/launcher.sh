e58ba5b49fb6df546d03107462c91ba374ca2137
wadaniel <wadaniel@ethz.ch>
2022-04-01 01:08:11 +0200

    wip

--- python/launcher.sh
+++ python/launcher.sh
@@ -1,8 +1,8 @@
-IC='turbulence'
-run='11'
+IC='sinus'
+run='12'
 NEX=2000000
-N=16
-NA=16
+N=32
+NA=32
 NDNS=512
 dt=0.001
 noise=0.01
@@ -11,14 +11,22 @@ iex=0.1
 seed=42
 esteps=500
 
-
 mkdir -p ./runs/
 
 cp launcher.sh "./runs/launcher${run}.sh"
 
 git diff > "./runs/gitdiff${run}.txt"
 
-python run-vracer-burger.py --ic $IC --run $run --NE $NEX --N $N --NA $NA --dt $dt --nu $nu --iex $iex --noise $noise --seed $seed --episodelength $esteps --NDNS $NDNS --specreward
-python run-vracer-burger.py --ic $IC --run $run --NE $NEX --N $N --NA $NA --dt $dt --nu $nu --iex $iex --noise $noise --seed $seed --episodelength $esteps --NDNS $NDNS --specreward --test
+python run-vracer-burger.py --ic $IC --run $run --NE $NEX \
+    --N $N --NA $NA --dt $dt --nu $nu \
+    --iex $iex --noise $noise --seed $seed \
+    --episodelength $esteps --NDNS $NDNS
+
+python run-vracer-burger.py --ic $IC --run $run --NE $NEX \
+    --N $N --NA $NA --dt $dt --nu $nu \
+    --iex $iex --noise $noise --seed $seed \
+    --episodelength $esteps --NDNS $NDNS \
+    --test
+
 python -m korali.rlview --dir "_result_${IC}_${N}_${NA}_${dt}_${nu}_${noise}_${seed}_${run}" --out "vracer${run}.png"
 
