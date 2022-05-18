823058246d279463becf10fdad833e735e87506c
Maximilian Guidi <max@dhcp-10-250-137-165.harvard.edu>
2022-05-18 15:28:35 -0400

    -

--- python/tests/ks/simulate.py
+++ python/tests/ks/simulate.py
@@ -2,7 +2,7 @@
 
 """
 This scripts simulates the KS on a fine grid (N) up to t=tEnd. We plot the KS and
-the instanteneous energy plus the time averaged energy, and the energy spectra at 
+the instanteneous energy plus the time averaged energy, and the energy spectra at
 start, mid and end of the simulation.
 """
 
@@ -17,41 +17,44 @@ sys.path.append('./../../_model/')
 import numpy as np
 from scipy.fft import fftfreq
 
-from KS import *
+from Kuramoto import *
 
 #------------------------------------------------------------------------------
 # DNS defaults
-N    = 1024
-L    = 22/(2*np.pi)
-nu   = 1.0
-dt   = 0.01
-tTransient = 0
-tEnd = 5000
-tSim = tEnd - tTransient
+N    = 124
+L    = 32*np.pi
+nu   = 1
+# dt   = 3*1e-6
+dt   = 0.25
+# tTransient = 2*1e-2
+# tEnd = 6*1e-3
+# tSim = tEnd
+tSim = 150
 nSimSteps = int(tSim/dt)
 
 #------------------------------------------------------------------------------
 ## transient
-dns = KS(L=L, N=N, dt=dt, nu=nu, tend=tTransient)
+dns = Kuramoto(L=L, N=N, dt=dt, nu=nu, tend=tSim, case='ETDRK4')
 dns.simulate()
 dns.fou2real()
- 
-#------------------------------------------------------------------------------
-## restart
-v_restart = dns.vv[-1,:].copy()
-u_restart = dns.uu[-1,:].copy()
- 
-# set IC
-dns.IC( v0 = v_restart )
-
-# continue simulation
-dns.simulate( nsteps=int(tSim/dt), restart = True )
-
-# convert to physical space
-dns.fou2real()
-
-# compute energies
 dns.compute_Ek()
+
+#------------------------------------------------------------------------------
+# ## restart
+# v_restart = dns.vv[-1,:].copy()
+# u_restart = dns.uu[-1,:].copy()
+#
+# # set IC
+# dns.IC( v0 = v_restart )
+#
+# # continue simulation
+# dns.simulate( nsteps=int(tSim/dt), restart = True )
+#
+# # convert to physical space
+# dns.fou2real()
+#
+# # compute energies
+# dns.compute_Ek()
 
 #------------------------------------------------------------------------------
 ## plot result
@@ -62,10 +65,13 @@ e_ktt = dns.Ek_ktt
 
 k = dns.k[:N//2]
 
-fig, axs = plt.subplots(1,3, figsize=(15,15))
-s, n = np.meshgrid(2*np.pi*L/N*(np.array(range(N))+1), dns.tt)
+#fig, axs = plt.subplots(1,3, figsize=(15,15))
+fig, axs = plt.subplots(1,3)
+print(dns.tt.shape)
+print(dns.x.shape)
+print(dns.uu.shape)
 
-axs[0].contourf(s, n, u, 50)
+axs[0].contourf(dns.x, dns.tt, dns.uu)
 axs[1].plot(dns.tt, e_t)
 axs[1].plot(dns.tt, e_tt)
 axs[2].plot(k, 2.0/N * np.abs(e_ktt[0,0:N//2]),'b:')
@@ -74,4 +80,5 @@ axs[2].plot(k, 2.0/N * np.abs(e_ktt[-1,0:N//2]),'b')
 axs[2].set_xscale('log')
 axs[2].set_yscale('log')
 
+print("Plotting simulate.png")
 fig.savefig('simulate.png')
