77d1f636ee81448309bd61fdde0b6cdd215ef6b8
wadaniel <wadaniel@ethz.ch>
2023-05-10 10:58:47 +0200

    added advection simple launcher

---
+++ python/runs/advection_simple_launcher.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+run=12864
+N=128
+numAgents=128
+noise=0.5
+episodelength=500
+iex=0.5
+exp=1000000
+
+dir=/scratch/wadaniel/advection_simple/run${run}/
+mkdir ${dir} -p
+
+launchname="${0##*/}"
+cp $launchname "${dir}/advection_simple_launcher_${run}.sh"
+git diff > "${dir}/advection_simple_gitdiff_${run}.txt"
+
+cd ..
+pushd .
+
+cp run-vracer-advection-simple.py ${dir}
+cp -r _model/ ${dir}
+
+cd ${dir}
+
+export OMP_NUM_THREADS=8
+python run-vracer-advection-simple.py --N ${N} --episodelen ${episodelength} \
+    --numAgents ${numAgents} --noise ${noise} --exp ${exp} --run ${run} --iex ${iex} \
+
+python run-vracer-advection-simple.py --N ${N} --episodelen ${episodelength} \
+    --numAgents ${numAgents} --noise ${noise} --exp ${exp} --run ${run} --iex ${iex} \
+    --test
+
+popd
+
+python3 -m korali.rlview --dir "${dir}/_result_advection_simple_${run}" --out "vracer_advection_simple_${run}.png" \
+    --showCI 0.8 --showObservations
